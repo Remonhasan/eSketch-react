034a127fb9572ddab9bfcc6f50f96cefd47e1770
remonhasan <GH007+remonhasan@users.noreply.github.com>
2022-07-08 08:31:30 -0700

    added-design-depencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Button } from 'react-bootstrap'
 
-function App() {
+export default function () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <Button variant="primary">Primary</Button>
+       <Button variant="primary"><i className='fa fa-home'></i></Button>
     </div>
-  );
+  )
 }
 
-export default App;
