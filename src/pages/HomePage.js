3606b161e2fd3f3a4fa462fb2fc31ba52d8d2ae1
remonhasan <GH007+remonhasan@users.noreply.github.com>
2022-07-11 22:04:35 +0600

    added-visitor-api-data

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { Fragment, Component } from 'react'
 import FooterDesktop from '../components/common/FooterDesktop'
 import NavMenuDesktop from '../components/common/NavMenuDesktop'
@@ -6,8 +7,20 @@ import Collection from '../components/home/Collection'
 import FeaturedProducts from '../components/home/FeaturedProducts'
 import HomeTop from '../components/home/HomeTop'
 import NewArrival from '../components/home/NewArrival'
+import ApiURL from '../api/ApiURL'
 
 export default class HomePage extends Component {
+
+    componentDidMount (){
+        window.scroll(0,0);
+        this.GetVisitorDetails();
+    }
+
+    // Catch VisitorDetails api infos
+    GetVisitorDetails = ()=>{
+        axios.get(ApiURL.VisitorDetails).then().catch();
+    }
+
     render() {
         return (
             <Fragment>
